e389d2422928789207c0b545e747b2a5cd205572
Menno Pieters <menno.pieters@everett.nl>
2016-10-24 13:18:24 +0200

    A plugin specific exception class based upon the GeneralException

---
+++ src/sailpoint/plugin/iiqmdmplugin/MasterDataManagementPluginException.java
@@ -0,0 +1,18 @@
+package sailpoint.plugin.iiqmdmplugin;
+
+import sailpoint.tools.GeneralException;
+
+public class MasterDataManagementPluginException extends GeneralException {
+	
+	public MasterDataManagementPluginException() {
+		super();
+	}
+	
+	public MasterDataManagementPluginException(Exception e) {
+		super(e);
+	}
+	
+	public MasterDataManagementPluginException(String s) {
+		super(s);
+	}
+}
